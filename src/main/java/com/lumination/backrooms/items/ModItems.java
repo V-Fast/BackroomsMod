e704bfb5b1f7abf62c4dd7c54168383b238a3180
u-lumaa <nathan@ama49.fr>
2022-08-21 00:04:00 +0200

    H a L L S - CG5

--- src/main/java/com/lumination/backrooms/items/ModItems.java
+++ src/main/java/com/lumination/backrooms/items/ModItems.java
@@ -1,20 +1,22 @@
 package com.lumination.backrooms.items;
 
 import com.lumination.backrooms.BackroomsMod;
-import com.lumination.backrooms.entities.ModEntities;
 import com.lumination.backrooms.items.drinks.AlmondWater;
 import com.lumination.backrooms.items.interactables.CameraItem;
-import com.lumination.backrooms.items.interactables.TestItem;
+import com.lumination.backrooms.sounds.ModSounds;
+import com.lumination.backrooms.items.interactables.MusicTapes;
 import net.fabricmc.fabric.api.item.v1.FabricItemSettings;
 import net.minecraft.entity.effect.StatusEffectInstance;
 import net.minecraft.entity.effect.StatusEffects;
 import net.minecraft.item.FoodComponent;
 import net.minecraft.item.Item;
-import net.minecraft.item.SpawnEggItem;
 import net.minecraft.util.Identifier;
+import net.minecraft.util.Rarity;
 import net.minecraft.util.registry.Registry;
 
 public class ModItems {
+    //public static final Item TESTITEM = registerItem("test_item", new TestItem(new FabricItemSettings().group(BackroomsItemsGroup.Main).maxCount(1)));
+
     public static final Item SILK = registerItem("silk",
             new Item(new FabricItemSettings().group(BackroomsItemsGroup.Main)));
     public static final Item SILKED_BOOK = registerItem("silked_book",
@@ -22,15 +24,12 @@ public class ModItems {
     public static final Item ALMOND_WATER = registerItem("almond_water", new AlmondWater(
             new FabricItemSettings().group(BackroomsItemsGroup.Main).maxCount(8).food(new FoodComponent.Builder().alwaysEdible().hunger(2*2).saturationModifier(3f).statusEffect(new StatusEffectInstance(StatusEffects.NAUSEA, 20 * 15, 0), 1F).build())));
     public static final Item COOKED_ALMOND_WATER = registerItem("cooked_almond_water", CookedAlmondWater(new FabricItemSettings().group(BackroomsItemsGroup.Main).maxCount(8).food(new FoodComponent.Builder().alwaysEdible().hunger(4*2).saturationModifier(4f).build())));
-    public static final Item ENTITY_SPAWN_EGG = registerItem("entity_spawn_egg",
-            new SpawnEggItem(ModEntities.ENTITY,0x0f0f0f, 0x1e1e1e,
-                    new FabricItemSettings().group(BackroomsItemsGroup.Main)));
     public static final Item TAPE = registerItem("tape", new Item(new FabricItemSettings().group(BackroomsItemsGroup.Main)));
     public static final Item CAMERA = registerItem("camera", new CameraItem(
             new FabricItemSettings().group(BackroomsItemsGroup.Main).maxCount(1)));
 
-
-    //public static final Item TESTITEM = registerItem("test_item", new TestItem(new FabricItemSettings().group(BackroomsItemsGroup.Main).maxCount(1)));
+    // discs always at the end
+    public static final Item HALLS_TAPE = registerItem("halls_tape", new MusicTapes(15, ModSounds.HALLS,new FabricItemSettings().group(BackroomsItemsGroup.Main).maxCount(1).rarity(Rarity.RARE), 166));
 
     public static AlmondWater CookedAlmondWater(Item.Settings settings) {
         return new AlmondWater(settings).setCooked(true);
