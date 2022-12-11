5b1d955720f5c815411001dbd1019e2d3ef07bb2
u-lumaa <nathan@ama49.fr>
2022-12-11 01:41:30 +0100

    uhhhh

--- src/main/java/com/lumination/backrooms/items/ModItemsClient.java
+++ src/main/java/com/lumination/backrooms/items/ModItemsClient.java
@@ -1,18 +1,10 @@
 package com.lumination.backrooms.items;
 
 import com.lumination.backrooms.BackroomsMod;
-import com.lumination.backrooms.items.drinks.AlmondWater;
-import com.lumination.backrooms.items.interactables.CameraItem;
-import com.lumination.backrooms.items.interactables.MusicTape;
-import com.lumination.backrooms.items.interactables.SilkenBook;
 import com.lumination.backrooms.items.interactables.SilkenBookClient;
-import com.lumination.backrooms.sounds.ModSounds;
 import net.fabricmc.api.EnvType;
 import net.fabricmc.api.Environment;
 import net.fabricmc.fabric.api.item.v1.FabricItemSettings;
-import net.minecraft.entity.effect.StatusEffectInstance;
-import net.minecraft.entity.effect.StatusEffects;
-import net.minecraft.item.FoodComponent;
 import net.minecraft.item.Item;
 import net.minecraft.util.Identifier;
 import net.minecraft.util.registry.Registry;
@@ -25,7 +17,7 @@ public class ModItemsClient {
     // Register
 
     public static Item registerItem(String name, Item item) {
-        
+
         return Registry.register(Registry.ITEM, new Identifier(BackroomsMod.MOD_ID, name), item);
     }
 
