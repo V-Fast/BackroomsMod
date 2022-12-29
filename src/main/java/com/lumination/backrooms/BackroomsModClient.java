0a78a5427be0416657680349f3e9f986732338b8
Lumaa <nathan@ama49.fr>
2022-12-29 13:13:21 +0100

    UpdateChecker implementation

--- src/main/java/com/lumination/backrooms/BackroomsModClient.java
+++ src/main/java/com/lumination/backrooms/BackroomsModClient.java
@@ -5,14 +5,19 @@ import com.lumination.backrooms.utils.ModRegisteries;
 import net.fabricmc.api.ClientModInitializer;
 import net.fabricmc.api.EnvType;
 import net.fabricmc.api.Environment;
+import net.fabricmc.loader.api.FabricLoader;
 
 import java.util.Date;
 
 @Environment(EnvType.CLIENT)
 public class BackroomsModClient implements ClientModInitializer {
     public static final CameraRecordHud camHud = new CameraRecordHud();
-    public static final String versionId = "A-0.0.3A";
-    public static final String versionName = "Developer (A-0.0.5A)";
+    public static final String versionId = FabricLoader.getInstance()
+            .getModContainer("backrooms")
+            .orElseThrow()
+            .getMetadata()
+            .getVersion()
+            .getFriendlyString();
     public static final boolean hasPrivateServer = false;
     public static long start;
 
