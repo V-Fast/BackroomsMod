31342d33cfd0e5c1985e62ce771660c619980ec9
u-lumaa <nathan@ama49.fr>
2022-12-31 19:06:27 +0100

    Adopted the LibuLib.Color.Backrooms.Blue

--- src/main/java/com/lumination/backrooms/mixin/TitleScreenMixin.java
+++ src/main/java/com/lumination/backrooms/mixin/TitleScreenMixin.java
@@ -1,14 +1,15 @@
 package com.lumination.backrooms.mixin;
 
+import com.lumaa.libu.Color;
 import com.lumination.backrooms.BackroomsMod;
 import com.lumination.backrooms.BackroomsModClient;
 import com.lumination.backrooms.client.Discord;
 import com.lumination.backrooms.client.screens.SettingsScreen;
+import net.fabricmc.loader.api.FabricLoader;
 import net.minecraft.client.gui.screen.Screen;
 import net.minecraft.client.gui.screen.TitleScreen;
 import net.minecraft.client.gui.widget.ButtonWidget;
 import net.minecraft.text.Text;
-import net.minecraft.util.Formatting;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
@@ -28,10 +29,13 @@ public class TitleScreenMixin extends Screen {
         Discord.setPresence("On the title screen", "", "async");
 
         int l = this.height / 4 + 48;
-        this.addDrawableChild(ButtonWidget.builder(Text.literal("A").formatted(Formatting.BLUE), (button) -> {
-            this.client.setScreen(new SettingsScreen().getScreen(this));
-        })
-                .dimensions(this.width / 2 + 104, l + 48, 20, 20)
-                .build());
+        Text a = Text.literal("A");
+        if (FabricLoader.getInstance().isModLoaded("libu")) {
+            this.addDrawableChild(ButtonWidget.builder(a.getWithStyle(a.getStyle().withColor(Color.Backrooms.blue)).get(0), (button) -> {
+                this.client.setScreen(new SettingsScreen().getScreen(this));
+            })
+                    .dimensions(this.width / 2 + 104, l + 48, 20, 20)
+                    .build());
+        }
     }
 }
