baaf9b7d4f91eddd2a806d93e5efd1a25901bd3c
Lumaa <nathan@ama49.fr>
2023-02-21 10:52:41 +0100

    better level 0 core

--- src/main/java/com/lumination/backrooms/items/interactables/TestItem.java
+++ src/main/java/com/lumination/backrooms/items/interactables/TestItem.java
@@ -2,10 +2,17 @@ package com.lumination.backrooms.items.interactables;
 
 import com.lumaa.libu.generation.GenerationCore;
 import com.lumaa.libu.generation.MazeCore;
+import com.lumaa.libu.util.BetterText;
+import com.lumaa.libu.util.Color;
 import com.lumination.backrooms.blocks.ModBlocks;
+import net.minecraft.client.item.TooltipContext;
 import net.minecraft.item.Item;
+import net.minecraft.item.ItemStack;
 import net.minecraft.item.ItemUsageContext;
+import net.minecraft.text.Text;
 import net.minecraft.util.ActionResult;
+import net.minecraft.world.World;
+import org.jetbrains.annotations.Nullable;
 
 import java.util.List;
 
@@ -25,5 +32,16 @@ public class TestItem extends Item {
         mazeCore.generate(context.getWorld(), context.getBlockPos());
 
         return ActionResult.PASS;
+    }
+
+    @Override
+    public boolean hasGlint(ItemStack stack) {
+        return true;
+    }
+
+    @Override
+    public void appendTooltip(ItemStack stack, @Nullable World world, List<Text> tooltip, TooltipContext context) {
+        tooltip.add(Text.empty());
+        tooltip.add(new BetterText("This is an experimental item.", BetterText.TextType.LITERAL).withColor(Color.brand));
     }
 }
