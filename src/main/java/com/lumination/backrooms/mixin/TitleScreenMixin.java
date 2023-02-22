b3c4229f9fccf745db01cb06eab9201692b620e1
Lumaa <nathan@ama49.fr>
2023-02-22 15:04:50 +0100

    cloth config not required

--- src/main/java/com/lumination/backrooms/mixin/TitleScreenMixin.java
+++ src/main/java/com/lumination/backrooms/mixin/TitleScreenMixin.java
@@ -11,7 +11,9 @@ import com.lumination.backrooms.client.screens.SettingsScreen;
 import net.fabricmc.loader.api.FabricLoader;
 import net.minecraft.client.gui.screen.Screen;
 import net.minecraft.client.gui.screen.TitleScreen;
+import net.minecraft.client.gui.tooltip.Tooltip;
 import net.minecraft.client.gui.widget.ButtonWidget;
+import net.minecraft.client.gui.widget.ClickableWidget;
 import net.minecraft.text.Text;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.injection.At;
@@ -20,6 +22,7 @@ import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
 
 @Mixin(TitleScreen.class)
 public class TitleScreenMixin extends Screen {
+    private static ClickableWidget settingsButton;
 
     protected TitleScreenMixin(Text title) {
         super(title);
@@ -34,11 +37,16 @@ public class TitleScreenMixin extends Screen {
 
         int l = this.height / 4 + 48;
         if (FabricLoader.getInstance().isModLoaded("libu")) {
-            this.addDrawableChild(ButtonWidget.builder(new BetterText("A", TextType.LITERAL).withColor(Color.Backrooms.blue), (button) -> {
+            this.settingsButton = this.addDrawableChild(ButtonWidget.builder(new BetterText("A", TextType.LITERAL).withColor(Color.Backrooms.blue), (button) -> {
                 this.client.setScreen(new SettingsScreen().getScreen(this));
             })
                     .dimensions(this.width / 2 + 104, l + 48, 20, 20)
                     .build());
+
+            this.settingsButton.active = FabricLoader.getInstance().isModLoaded("cloth_config");
+            if (!FabricLoader.getInstance().isModLoaded("cloth_config")) {
+                this.settingsButton.setTooltip(Tooltip.of(Text.translatable("mod.cc.disabled")));
+            }
         }
     }
 }
