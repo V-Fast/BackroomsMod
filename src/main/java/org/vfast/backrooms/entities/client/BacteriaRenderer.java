be4ab044487726c19a6e062b47de619354bce97a
Clomclem <clemheiremans@gmail.com>
2023-11-29 19:59:29 -0300

    Smiler progress

--- src/main/java/org/vfast/backrooms/entities/client/BacteriaRenderer.java
+++ src/main/java/org/vfast/backrooms/entities/client/BacteriaRenderer.java
@@ -1,10 +1,8 @@
 package org.vfast.backrooms.entities.client;
 
-import org.vfast.backrooms.BackroomsMod;
 import org.vfast.backrooms.entities.BacteriaEntity;
 
 import net.minecraft.client.render.entity.EntityRendererFactory;
-import net.minecraft.util.Identifier;
 import org.quiltmc.loader.api.minecraft.ClientOnly;
 import software.bernie.geckolib.renderer.GeoEntityRenderer;
 
@@ -12,10 +10,5 @@ import software.bernie.geckolib.renderer.GeoEntityRenderer;
 public class BacteriaRenderer extends GeoEntityRenderer<BacteriaEntity> {
     public BacteriaRenderer(EntityRendererFactory.Context ctx) {
         super(ctx, new BacteriaModel());
-    }
-
-    @Override
-    public Identifier getTextureLocation(BacteriaEntity object) {
-        return new Identifier(BackroomsMod.MOD_ID, "textures/entity/bacteria.png");
     }
 }
