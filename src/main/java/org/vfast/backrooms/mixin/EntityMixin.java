2242e66265806e6e23b9d4cfb8b3430b617eb8c1
Clomclem <clemheiremans@gmail.com>
2023-12-22 19:22:28 -0300

    Removed random chance to get teleported to the Backrooms, might re-add later as a configurable option

--- src/main/java/org/vfast/backrooms/mixin/EntityMixin.java
+++ src/main/java/org/vfast/backrooms/mixin/EntityMixin.java
@@ -25,6 +25,7 @@ import org.vfast.backrooms.world.BackroomsDimensions;
 
 @Mixin(Entity.class)
 public abstract class EntityMixin implements Nameable, EntityLike, CommandOutput, IEntityDataAccessor {
+    @Unique
     private NbtCompound persistentData;
     @Unique
     private static final String NBT_ID = BackroomsMod.ID+".data";
@@ -54,7 +55,7 @@ public abstract class EntityMixin implements Nameable, EntityLike, CommandOutput
     @Inject(method = "tick", at = @At("TAIL"))
     private void onTick(CallbackInfo ci) {
         Random rand = ((Entity)(Object)this).getWorld().getRandom();
-        if ((((Entity)(Object)this).isInsideWall() && rand.nextBetween(1, 50) == 50) || (rand.nextBetween(1, 36000) == 36000 && ((Entity)(Object)this).isPlayer() && ((Entity)(Object)this).getWorld() != ((Entity)(Object)this).getServer().getWorld(BackroomsDimensions.LEVEL_ZERO_KEY) && !(((ServerPlayerEntity)((Entity)(Object)this)).isCreative() || ((ServerPlayerEntity)((Entity)(Object)this)).isSpectator()))) {
+        if (((Entity)(Object)this).isInsideWall() && rand.nextBetween(1, 50) == 50) {
             LimlibTravelling.travelTo(((Entity)(Object)this), ((Entity)(Object)this).getServer().getWorld(BackroomsDimensions.LEVEL_ZERO_KEY), new TeleportTarget(
                             Vec3d.of(new Vec3i(rand.nextBetween(((Entity)(Object)this).getBlockX()-200, ((Entity)(Object)this).getBlockX()+200), 2, rand.nextBetween(((Entity)(Object)this).getBlockZ()-200, ((Entity)(Object)this).getBlockZ()+200))),
                             Vec3d.ZERO, 0.0f, 0.0f),
