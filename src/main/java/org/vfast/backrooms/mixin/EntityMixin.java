7b104d28fd02f87e212bb59af0f3061b0ce1d57a
joshuaday <joshmikeday@gmail.com>
2024-07-25 10:35:30 +0100

    update gradle, geckolib, loom, and fabric loader, and fix and clean EntityMixin to prevent crash when getServer returns null

--- src/main/java/org/vfast/backrooms/mixin/EntityMixin.java
+++ src/main/java/org/vfast/backrooms/mixin/EntityMixin.java
@@ -4,13 +4,16 @@ import net.ludocrypt.limlib.api.LimlibTravelling;
 import net.minecraft.entity.Entity;
 import net.minecraft.server.command.CommandOutput;
 import net.minecraft.server.network.ServerPlayerEntity;
+import net.minecraft.server.world.ServerWorld;
 import net.minecraft.util.Nameable;
 import net.minecraft.util.math.Vec3d;
 import net.minecraft.util.math.Vec3i;
 import net.minecraft.util.math.random.Random;
 import net.minecraft.world.TeleportTarget;
 import net.minecraft.world.entity.EntityLike;
+import org.spongepowered.asm.mixin.Final;
 import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.Shadow;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
@@ -19,12 +22,17 @@ import org.vfast.backrooms.world.BackroomsDimensions;
 
 @Mixin(Entity.class)
 public abstract class EntityMixin implements Nameable, EntityLike, CommandOutput {
+    @Shadow
+    @Final
+    protected Random random;
     @Inject(method = "tick", at = @At("TAIL"))
     private void onTick(CallbackInfo ci) {
-        Random rand = ((Entity)(Object)this).getWorld().getRandom();
-        if ((((Entity)(Object)this).isInsideWall() && rand.nextBetween(1, 50) == 50) || (rand.nextBetween(1, 36000) == 36000 && ((Entity)(Object)this).isPlayer() && ((Entity)(Object)this).getWorld() != ((Entity)(Object)this).getServer().getWorld(BackroomsDimensions.LEVEL_ZERO_KEY) && !(((ServerPlayerEntity)((Entity)(Object)this)).isCreative() || ((ServerPlayerEntity)((Entity)(Object)this)).isSpectator()))) {
-            LimlibTravelling.travelTo(((Entity)(Object)this), ((Entity)(Object)this).getServer().getWorld(BackroomsDimensions.LEVEL_ZERO_KEY), new TeleportTarget(
-                            Vec3d.of(new Vec3i(rand.nextBetween(((Entity)(Object)this).getBlockX()-200, ((Entity)(Object)this).getBlockX()+200), 2, rand.nextBetween(((Entity)(Object)this).getBlockZ()-200, ((Entity)(Object)this).getBlockZ()+200))),
+        Entity This = ((Entity)(Object)this);
+        if (!(This.getWorld() instanceof ServerWorld world)) return;
+        if (world == world.getServer().getWorld(BackroomsDimensions.LEVEL_ZERO_KEY)) return;
+        if ((This.isInsideWall() && random.nextBetween(1, 50) == 50) || (This.isPlayer() && !(((ServerPlayerEntity)This).isCreative() || This.isSpectator()) && random.nextBetween(1, 36000) == 36000)) {
+            LimlibTravelling.travelTo(This, world.getServer().getWorld(BackroomsDimensions.LEVEL_ZERO_KEY), new TeleportTarget(
+                            Vec3d.of(new Vec3i(random.nextBetween(This.getBlockX()-200, This.getBlockX()+200), 2, random.nextBetween(This.getBlockZ()-200, This.getBlockZ()+200))),
                             Vec3d.ZERO, 0.0f, 0.0f),
                     BackroomsSounds.CAMERA_CLICK, 5.0f, 1.0f);
         }
