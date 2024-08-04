e3cc901477283e7f4326a0d7556b88c67ed04da9
Lumaa <lumaa@lumaa.fr>
2024-08-04 16:37:03 +0200

    full rewrite begin

--- src/main/java/org/vfast/backrooms/item/BackroomsItemsGroup.java
+++ src/main/java/org/vfast/backrooms/item/BackroomsItemsGroup.java
@@ -20,26 +20,15 @@ public class BackroomsItemsGroup {
         .displayName(Text.translatable("itemGroup.backrooms.backrooms"))
     ));
 
-    public static final RegistryKey<ItemGroup> WEAPONS = BackroomsItemsGroup.createItemGroup("weapons", (builder -> builder
-            .icon(() -> new ItemStack(BackroomsItems.NAILED_BAT))
-            .displayName(Text.translatable("itemGroup.backrooms.weapons"))
-    ));
-
     public static final RegistryKey<ItemGroup> MUSIC_TAPES = BackroomsItemsGroup.createItemGroup("music_tapes", (builder -> builder
             .icon(() -> new ItemStack(BackroomsItems.TAPE))
             .displayName(Text.translatable("itemGroup.backrooms.music_tapes"))
     ));
 
-    public static final RegistryKey<ItemGroup> ENTITIES = BackroomsItemsGroup.createItemGroup("entity", (builder -> builder
-            .icon(() -> new ItemStack(BackroomsItems.BACTERIA_EGG))
-            .displayName(Text.translatable("itemGroup.backrooms.entities"))
-    ));
-
     public static RegistryKey<ItemGroup> createItemGroup(String idPath, Function<ItemGroup.Builder, ItemGroup.Builder> builderSupplier) {
-
         ItemGroup.Builder itemGroupBuilder = builderSupplier.apply(FabricItemGroup.builder());
 
-        RegistryKey<ItemGroup> itemGroupKey = RegistryKey.of(RegistryKeys.ITEM_GROUP, new Identifier(BackroomsMod.ID, idPath));
+        RegistryKey<ItemGroup> itemGroupKey = RegistryKey.of(RegistryKeys.ITEM_GROUP, Identifier.of(BackroomsMod.ID, idPath));
 
         Registry.register(Registries.ITEM_GROUP, itemGroupKey, itemGroupBuilder.build());
 
