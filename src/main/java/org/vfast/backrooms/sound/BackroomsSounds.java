5ca8aedd651425a12c91aa5808d68f85104e50d7
PatateGivree <jeanbaptiste.destaebel@gmail.com>
2024-11-27 21:57:33 +0100

    Trying to fix the ambient for level 94

--- src/main/java/org/vfast/backrooms/sound/BackroomsSounds.java
+++ src/main/java/org/vfast/backrooms/sound/BackroomsSounds.java
@@ -18,7 +18,7 @@ public class BackroomsSounds {
     public static SoundEvent CURIOSITY = registerSoundEvent("curiosity");
 
     // Ambient sounds
-    public static final SoundEvent LEVEL_94_AMBIENCE = registerSoundEvent("level_94_ambience");
+    public static final SoundEvent ROOM_WIND = registerSoundEvent("room_wind");
 
     private static SoundEvent registerSoundEvent(String name) {
         Identifier id = Identifier.of(BackroomsMod.ID, name);
