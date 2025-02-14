705d21069d45ccde6a3581f512ffdce8e2b418d9
lumaa-dev <lumaa@lumaa.fr>
2025-02-14 20:04:56 +0100

    default panorama resource pack

--- src/main/java/org/vfast/backrooms/BackroomsModClient.java
+++ src/main/java/org/vfast/backrooms/BackroomsModClient.java
@@ -6,6 +6,7 @@ import net.fabricmc.api.Environment;
 import net.fabricmc.fabric.api.resource.ResourceManagerHelper;
 import net.fabricmc.fabric.api.resource.ResourcePackActivationType;
 import net.fabricmc.loader.api.FabricLoader;
+import net.minecraft.text.Text;
 import net.minecraft.util.Identifier;
 
 @Environment(EnvType.CLIENT)
@@ -13,7 +14,8 @@ public class BackroomsModClient implements ClientModInitializer {
     @Override
     public void onInitializeClient() {
         FabricLoader.getInstance().getModContainer(BackroomsMod.ID).ifPresent(modContainer -> {
-            ResourceManagerHelper.registerBuiltinResourcePack(Identifier.of(BackroomsMod.ID, "old_ssc"), modContainer, ResourcePackActivationType.NORMAL);
+            ResourceManagerHelper.registerBuiltinResourcePack(Identifier.of(BackroomsMod.ID, "old_ssc"), modContainer, Text.translatable("resourcePack.old_ssc.name"), ResourcePackActivationType.NORMAL);
+            ResourceManagerHelper.registerBuiltinResourcePack(Identifier.of(BackroomsMod.ID, "def_pano"), modContainer, Text.translatable("resourcePack.def_pano.name"), ResourcePackActivationType.NORMAL);
         });
         BackroomsMod.LOGGER.info("Initialized Client Backrooms");
     }
